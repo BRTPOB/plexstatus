51005abce9fc8494c313bf8b92d983d6c34b6285
Offspring <offspring@eskimomafia.net>
2017-04-30 19:37:20 -0700

    Second commit

---
+++ assets/php/now_playing_title_ajax.php
@@ -0,0 +1,41 @@
+<?php
+	Ini_Set( 'display_errors', true );
+	include '../../init.php';
+	include 'functions.php';
+
+	// This is separate from the now_playing div because the now_playing div
+	// is a special scrollable div and we don't want the title scrolling with it.
+	// You will only notice the scrolling feature when there are multiple
+	// shows being watched at the same time.
+global $plex_instances;
+foreach ($plex_instances as $instance) {
+	
+	$plex_port = $instance[1];
+    /*
+    echo " instance var 0".$instance[0];
+    echo " instance var 1".$instance[1];
+    echo " instance var 2".$instance[2];
+    echo " instance var 3".$instance[3];
+    echo " instance var 4".$instance[4];
+	*/
+    $network = getNetwork($instance[3]);
+
+	$plexSessionXML = simplexml_load_file($network.':'.$plex_port.'/status/sessions');
+
+	// See if Plex Media Server is online and how many people are watching.
+	if (!$plexSessionXML):
+		// If Plex Media Server is offline.
+		$title = 'Recently Viewed';
+	else:
+		// If Plex Media Server is online.
+		if (count($plexSessionXML->Video) == 0):
+			$title = 'Recently Aired';
+		else:
+			$title = 'Now Playing';
+		endif;
+	endif;
+
+	echo '<h1 class="exoextralight">'.$title.'</h1>';
+	echo '<hr>';
+}
+?>
---
+++ assets/php/services_ajax.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<?php
+	Ini_Set( 'display_errors', true );
+	include '../../init.php';
+	include ROOT_DIR . '/assets/php/functions.php';
+	include("service.class.php");
+	include("serviceSAB.class.php");
+	include("serviceMinecraft.class.php");
+?>
+<html lang="en">
+	<script>
+	// Enable bootstrap tooltips
+	$(function ()
+	        { $("[rel=tooltip]").tooltip();
+	        });
+	</script>
+<?php
+/*
+$sabnzbdXML = simplexml_load_file('http://'.$sabnzbd_ip.':'.$sabnzbd_port.'/api?mode=qstatus&output=xml&apikey='.$sabnzbd_api);
+
+
+if (isset($sabnzbdXML) && ($sabnzbdXML->state) == 'Downloading'):
+	$timeleft = $sabnzbdXML->timeleft;
+	$sabTitle = 'SABnzbd ('.$timeleft.')';
+else:
+	$sabTitle = 'SABnzbd';
+endif;
+*/
+
+/*
+$services = array(
+	new service($plex_server_name, $plex_port, $plex_URL, $plex_ip),
+	//new service($pfSense_server_name, $pfSense_port, $pfSense_URL, $pfSense_ip),
+	new serviceSAB($sabTitle, $sabnzbd_port, $sabnzbd_URL, $sabnzbd_ip),
+	new service($sickbeard_server_name, $sickbeard_port, $sickbeard_URL, $sickbeard_ip),
+	new service($couchpotato_server_name, $couchpotato_port, $couchpotato_URL, $couchpotato_ip),
+	//new service("Transmission", 9091, "http://d4rk.co:9091", "10.0.1.5"),
+	//new service("iTunes Server", 3689, "http://www.apple.com/itunes/"),
+	//new service("Starbound Server", 21025, "http://playstarbound.com"),
+	//new serviceMinecraft("Vanilla", 25564, "http://minecraft.d4rk.co", "mc.d4rk.co"),
+	//new serviceMinecraft("Bevo Tech Pack", 25565, "http://minecraft.d4rk.co")
+);
+*/
+
+//I'm experimenting with using objects to track services in funcitons.php
+
+global $services;
+
+function add_service($instances) {
+	global $services;
+	
+	foreach ($instances as $instance) {
+		$temp_object = new service(array_values($instance)[0],array_values($instance)[1],array_values($instance)[2],array_values($instance)[3]);
+		$services[] = $temp_object;
+	}
+}
+	
+foreach ($service_instances as $service_instance) {
+	add_service($service_instance);
+}
+
+
+?>
+<table class="center">
+	<?php foreach($services as $service){ ?>
+		<tr>
+			<td style="text-align: right; padding-right:5px;" class="exoextralight"><?php echo $service->name; ?></td>
+			<td style="text-align: left;"><?php echo $service->makeButton(); ?></td>
+		</tr>
+	<?php }?>
+</table>
